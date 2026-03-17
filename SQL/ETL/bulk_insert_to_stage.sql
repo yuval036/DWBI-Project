9c9c2fb53b44d8cfa62cb081898f8e20974ce786
yuval036 <167618488+yuval036@users.noreply.github.com>
2026-03-17 13:39:22 +0200

    Update bulk_insert_to_stage.sql

--- SQL/ETL/bulk_insert_to_stage.sql
+++ SQL/ETL/bulk_insert_to_stage.sql
@@ -1,1 +1,40 @@
+------------------------------------------------------------------------------
+--
+------------------------------------------------------------------------------
+BULK INSERT stage.DimCustomerRow
+FROM 'C:\Users\yuval\OneDrive\שולחן העבודה\DWBI PROJECT\Data\DimCustomers.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+);
 
+INSERT INTO dim.DimCustomer
+(
+    FirstName,
+    LastName,
+    Gender,
+    DateOfBirth,
+    Email,
+    PhoneNumber,
+    [Address],
+    City,
+    [State],
+    Country,
+    ZipCode,
+    LoyaltyProgramID
+)
+SELECT
+    FirstName,
+    LastName,
+    Gender,
+    DateOfBirth,
+    Email,
+    PhoneNumber,
+    [Address],
+    City,
+    [State],
+    Country,
+    ZipCode,
+    LoyaltyProgramID
+FROM stage.DimCustomerRow;
