13a63e2a9ea7ea413cf3988fe4cd303d55146c96
yuval036 <167618488+yuval036@users.noreply.github.com>
2026-03-17 14:20:58 +0200

    Update bulk_insert_to_stage.sql

--- SQL/ETL/bulk_insert_to_stage.sql
+++ SQL/ETL/bulk_insert_to_stage.sql
@@ -42,5 +42,35 @@ WITH (
     ROWTERMINATOR = '\n'
 );
 
+------------------------------------------------------------------------------
+--bulk insert into dim.DimDate + insert a new date
+------------------------------------------------------------------------------
+BULK INSERT dim.DimDate
+FROM 'C:\Users\yuval\OneDrive\שולחן העבודה\DWBI PROJECT\Data\DimDate.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n'
+);
 
+INSERT INTO dim.DimDate
+(
+    DateID,
+    [Date],
+    DayOfWeek,
+    [Month],
+    [Quarter],
+    [Year],
+    IsWeekend
+)
+VALUES
+(
+    20260101,
+    '2026-01-01',
+    4,
+    1,
+    1,
+    2026,
+    0
+);
 
